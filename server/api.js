5d0cb2a4f9ee13038d88c310bd96da6ce87048b5
Vincent <v.leboulenger@outlook.fr>
2023-03-31 19:22:49 +0200

    feat(change-api): Changed the api so that we can specify a page for the products displayed

--- server/api.js
+++ server/api.js
@@ -28,13 +28,15 @@ app.get('/products/search', (request, response) => {
 	var limit = request.query.limit;
 	const brand = request.query.brand;
 	const price = request.query.price;
+	var page = request.query.page;
 	var result = {};
 	var searchProducts = [...marketplace];
+	if (page == undefined) {page = 1;};
 	if (price != undefined) {searchProducts = searchProducts.filter(product => product.price < price)};
 	if (brand != undefined) {searchProducts = searchProducts.filter(product => product.brand == brand)};
 	if (limit == undefined) {limit = 12};
 	result['limit'] = parseInt(limit);
-	searchProducts = searchProducts.slice(0,limit);
+	searchProducts = searchProducts.slice(limit * (page-1), limit * page);
 	result['total'] = searchProducts.length;
 	result['results'] = searchProducts;
 	response.status(200).json(result);
