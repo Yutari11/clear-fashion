d2196a5d39740912f945f9eb0b8dbc83b26285de
Vincent <v.leboulenger@outlook.fr>
2023-03-31 16:29:49 +0200

    feat(api): finished the API with Node.js

--- server/api.js
+++ server/api.js
@@ -2,6 +2,8 @@ const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
 
+const {marketplace} = require('./data.js');
+
 const PORT = 8092;
 
 const app = express();
@@ -17,6 +19,35 @@ app.options('*', cors());
 app.get('/', (request, response) => {
   response.send({'ack': true});
 });
+
+app.get('/products', (request, response) => {
+  response.status(200).json(marketplace);
+});
+
+app.get('/products/search', (request, response) => {
+	var limit = request.query.limit;
+	const brand = request.query.brand;
+	const price = request.query.price;
+	var result = {};
+	var searchProducts = [...marketplace];
+	if (price != undefined) {searchProducts = searchProducts.filter(product => product.price < price)};
+	if (brand != undefined) {searchProducts = searchProducts.filter(product => product.brand == brand)};
+	if (limit == undefined) {limit = 12};
+	result['limit'] = parseInt(limit);
+	searchProducts = searchProducts.slice(0,limit);
+	result['total'] = searchProducts.length;
+	result['results'] = searchProducts;
+	response.status(200).json(result);
+});
+
+app.get('/products/:id', (request, response) => {
+	const id = request.params.id;
+	//console.log(id);
+	const product = marketplace.find(product => product.uuid == id);
+	response.status(200).json(product);
+});
+
+
 
 app.listen(PORT);
 
